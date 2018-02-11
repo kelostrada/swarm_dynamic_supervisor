728b0f2053be4df5e2062359b61f078d65ba34cf
Bartosz Kalinowski <kelostrada@gmail.com>
2018-02-11 16:29:48 +0100

    added docs and hex publish

--- mix.exs
+++ mix.exs
@@ -7,14 +7,19 @@ defmodule Swarm.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: "Supervisor for Swarm registered processes to handle process crashes like regular DynamicSupervisor",
+      deps: deps(),
+      package: package(),
+      # Docs
+      name: "Swarm.DynamicSupervisor",
+      source_url: "https://github.com/kelostrada/swarm_dynamic_supervisor",
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: []
     ]
   end
 
@@ -22,6 +27,14 @@ defmodule Swarm.MixProject do
   defp deps do
     [
       {:swarm, ">= 3.0.0"},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false}
     ]
+  end
+
+  defp package do
+    [files: ["lib", "mix.exs", "README.md"],
+     maintainers: ["Bartosz Kalinowski"],
+     licenses: ["MIT"],
+     links: %{ "Github": "https://github.com/kelostrada/swarm_dynamic_supervisor" }]
   end
 end
